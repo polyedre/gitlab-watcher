d2ef89c24f04ab0ebd3053892fc63e948fb9ae26
polyedre <polyedre@disroot.org>
2021-04-08 22:42:36 +0200

    style(display+utils): abstracted elements with dataclass

--- gitlab_watcher/display.py
+++ gitlab_watcher/display.py
@@ -1,24 +1,43 @@
 """Generic print function for Gitlab's elements"""
 
+from dataclasses import asdict, dataclass
 from colorama import Fore, Style
+from gitlab_watcher.utils import GitlabElement
 
 
 def pretty_print_gitlab_list(elements, name):
     """Print a section named `name` containing all the `elements`.
     For each element, its title, the relative date it was last updated at and
     the related project name are printed."""
+    if not elements:
+        return
+
+    fields_length = get_column_width(GitlabElement, elements)
+
+    colorised_elements = (
+        GitlabColoredElement(
+            **{key: ColorisedString(value) for key, value in asdict(element).items()}
+        )
+        for element in elements
+    )
+
     print_section_title(f"{name} ({len(elements)})")
-    for element in elements:
-        element.title_and_labels = (
-            f"{Style.BRIGHT}{Fore.YELLOW if element.title.startswith('Draft: ') else Fore.GREEN}{element.title}   "
-            f"{Style.DIM}{Fore.RED}{', '.join(element.labels)}{Style.RESET_ALL}"
-        )
+    for element in colorised_elements:
         print(
-            f"{element.project.name:20.20s}",
-            f"{element.title_and_labels:120.120s}",
-            f"{Style.DIM}{element.human_relative_time:12.12s}{Style.RESET_ALL}",
-            f"{Fore.BLUE}{Style.DIM}{element.web_url}{Style.RESET_ALL}",
+            " ".join(
+                getattr(element, field).to_string(length) if length else ""
+                for field, length in fields_length.items()
+            )
         )
+
+
+def get_column_width(dataclass_class, elements):
+    """Return a dictionnary that associate each field with the lenght of the
+    biggest field"""
+    return {
+        field: max((len(element.__getattribute__(field)) for element in elements))
+        for field in dataclass_class.__dataclass_fields__
+    }
 
 
 def print_section_title(name):
@@ -29,3 +48,48 @@ def print_section_title(name):
 def section_separator():
     """Print the separator between 2 sections"""
     print()
+
+
+class ColorisedString:
+    """Class to add colors and keep the right length"""
+
+    def __init__(self, content, style="", color=""):
+        self.content = content
+        self.style = style
+        self.color = color
+
+    def __len__(self):
+        """Return the content length"""
+        return len(self.content)
+
+    def to_string(self, length=None):
+        """Return a string with color codes included"""
+        if length:
+            fstring = (
+                f"{self.style}{self.color}{{:{length}.{length}s}}{Style.RESET_ALL}"
+            )
+        else:
+            fstring = f"{self.style}{self.color}{{}}{Style.RESET_ALL}"
+        return fstring.format(self.content)
+
+
+@dataclass
+class GitlabColoredElement:
+    """Class for keeping track of a Gitlab element, handle colors."""
+
+    project: ColorisedString
+    title: ColorisedString
+    labels: ColorisedString
+    updated_at: ColorisedString
+    url: ColorisedString
+
+    def __post_init__(self):
+        self.title.style = Style.BRIGHT
+        self.title.color = (
+            Fore.YELLOW if self.title.content.startswith("Draft: ") else Fore.GREEN
+        )
+        self.labels.style = Style.DIM
+        self.labels.color = Fore.RED
+        self.updated_at.style = Style.DIM
+        self.url.style = Style.DIM
+        self.url.color = Fore.BLUE
--- gitlab_watcher/utils.py
+++ gitlab_watcher/utils.py
@@ -1,6 +1,8 @@
 """Utilities used by other modules"""
 
+from dataclasses import dataclass
 from datetime import datetime
+
 from dateutil.parser import parse
 from humanize import naturaltime
 
@@ -10,13 +12,7 @@ projects_cache = {}
 def enrich_gitlab_list(elements, gitlab_api):
     """Add `project`, `updated_at_datetime` and `human_relative_time` attribute
     to all the elements"""
-    for element in elements:
-        element.project = get_project_by_id(element.project_id, gitlab_api)
-        element.updated_at_datetime = parse(element.updated_at, ignoretz=True)
-        element.human_relative_time = naturaltime(
-            element.updated_at_datetime, when=datetime.utcnow()
-        )
-
+    elements = (GitlabElement(element, gitlab_api) for element in elements)
     return sorted(elements, key=lambda i: i.updated_at_datetime, reverse=True)
 
 
@@ -26,3 +22,24 @@ def get_project_by_id(identifier, gitlab_api):
     if identifier not in projects_cache:
         projects_cache[identifier] = gitlab_api.projects.get(identifier)
     return projects_cache[identifier]
+
+
+@dataclass
+class GitlabElement:
+    """Class for keeping track of a Gitlab element, handle colors."""
+
+    project: str
+    title: str
+    labels: str
+    updated_at: str
+    url: str
+
+    def __init__(self, element, gitlab_api):
+        self.title = element.title
+        self.project = get_project_by_id(element.project_id, gitlab_api).name
+        self.updated_at_datetime = parse(element.updated_at, ignoretz=True)
+        self.updated_at = (
+            naturaltime(self.updated_at_datetime, when=datetime.utcnow()) or ""
+        )
+        self.labels = ", ".join(element.labels)
+        self.url = element.web_url
