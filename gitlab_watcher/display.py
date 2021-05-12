5bb7753312bdbb90868d410830b9b2b7fb2248d7
Polyedre <polyedre@disroot.org>
2021-05-12 18:31:58 +0200

    ui: use rich tables
    This adds a new dependency but reduces the complexity of the ui. It is also
    easier to read.

--- gitlab_watcher/display.py
+++ gitlab_watcher/display.py
@@ -1,95 +1,34 @@
 """Generic print function for Gitlab's elements"""
 
-from dataclasses import asdict, dataclass
-from colorama import Fore, Style
 from gitlab_watcher.utils import GitlabElement
+from rich.console import Console
+from rich.table import Table
+from rich import box
 
 
-def pretty_print_gitlab_list(elements, name):
+def pretty_print_gitlab_list(elements: list[GitlabElement], name):
     """Print a section named `name` containing all the `elements`.
     For each element, its title, the relative date it was last updated at and
     the related project name are printed."""
     if not elements:
         return
 
-    fields_length = get_column_width(GitlabElement, elements)
+    table = Table(title=name, box=box.ROUNDED)
 
-    colorised_elements = (
-        GitlabColoredElement(
-            **{key: ColorisedString(value) for key, value in asdict(element).items()}
+    table.add_column("Project", justify="right", style="cyan", no_wrap=True)
+    table.add_column("Title", style="magenta")
+    table.add_column("Labels", justify="center", style="green")
+    table.add_column("Updated at", justify="left", style="green")
+    table.add_column("URL", justify="left", style="blue")
+
+    for element in elements:
+        table.add_row(
+            element.project,
+            element.title,
+            element.labels,
+            element.updated_at,
+            element.url,
+            style="dim" if element.title.startswith("Draft:") else "bold",
         )
-        for element in elements
-    )
 
-    print_section_title(f"{name} ({len(elements)})")
-    for element in colorised_elements:
-        print(
-            " ".join(
-                getattr(element, field).to_string(length) if length else ""
-                for field, length in fields_length.items()
-            )
-        )
-
-
-def get_column_width(dataclass_class, elements):
-    """Return a dictionnary that associate each field with the lenght of the
-    biggest field"""
-    return {
-        field: max((len(element.__getattribute__(field)) for element in elements))
-        for field in dataclass_class.__dataclass_fields__
-    }
-
-
-def print_section_title(name):
-    """Print the title of a new section"""
-    print(f"{Style.BRIGHT}{Fore.MAGENTA}{name}{Style.RESET_ALL}")
-
-
-def section_separator():
-    """Print the separator between 2 sections"""
-    print()
-
-
-class ColorisedString:
-    """Class to add colors and keep the right length"""
-
-    def __init__(self, content, style="", color=""):
-        self.content = content
-        self.style = style
-        self.color = color
-
-    def __len__(self):
-        """Return the content length"""
-        return len(self.content)
-
-    def to_string(self, length=None):
-        """Return a string with color codes included"""
-        if length:
-            fstring = (
-                f"{self.style}{self.color}{{:{length}.{length}s}}{Style.RESET_ALL}"
-            )
-        else:
-            fstring = f"{self.style}{self.color}{{}}{Style.RESET_ALL}"
-        return fstring.format(self.content)
-
-
-@dataclass
-class GitlabColoredElement:
-    """Class for keeping track of a Gitlab element, handle colors."""
-
-    project: ColorisedString
-    title: ColorisedString
-    labels: ColorisedString
-    updated_at: ColorisedString
-    url: ColorisedString
-
-    def __post_init__(self):
-        self.title.style = Style.BRIGHT
-        self.title.color = (
-            Fore.YELLOW if self.title.content.startswith("Draft: ") else Fore.GREEN
-        )
-        self.labels.style = Style.DIM
-        self.labels.color = Fore.RED
-        self.updated_at.style = Style.DIM
-        self.url.style = Style.DIM
-        self.url.color = Fore.BLUE
+    Console().print(table)
